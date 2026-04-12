0e5702aab4a1afde500233a7e02a3d48363f39df
Mark Hoerth <mhoerth@dremio.com>
2026-04-12 10:44:37 -0700

    Fix Iceberg support for standard Apache Spark 3.5.5

--- spark-sql/start-spark-sql.sh
+++ spark-sql/start-spark-sql.sh
@@ -3,4 +3,8 @@ exec /opt/spark/bin/spark-sql \
   --master local[*] \
   --conf spark.plugins=org.apache.gravitino.spark.connector.plugin.GravitinoSparkPlugin \
   --conf spark.sql.gravitino.uri=http://gqs-gravitino:8090 \
-  --conf spark.sql.gravitino.metalake=demo
+  --conf spark.sql.gravitino.metalake=demo \
+  --conf spark.sql.gravitino.enableIcebergSupport=true \
+  --conf spark.sql.catalog.iceberg_nyc.s3.endpoint=http://gqs-minio:9000 \
+  --conf spark.sql.catalog.iceberg_nyc.s3.path-style-access=true \
+  --conf spark.sql.catalog.iceberg_nyc.s3.region=us-east-1
